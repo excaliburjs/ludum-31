fb25d2b67283ccfef994735fea6f2db5f21903f0
Travis-CI <travis@excaliburjs.com>
2014-12-11 06:50:13 +0000

    Deploying game for 22f542e4c8f0479d9b3cca6b715fb4cb94021b36

--- game/mainmenu.ts
+++ game/mainmenu.ts
@@ -54,11 +54,7 @@ class MainMenu extends ex.UIActor {
          if (slides.length === (tutNormalIdx + 1)) {
             tutNormalIdx = 0;
             document.getElementById("tutorial-normal-next").innerHTML = "Next";
-            this._dismissNormalTutorial();
-            if (MainMenu._LoadAfterTutorial) {
-               MainMenu._LoadAfterTutorial = false;
-               MainMenu.LoadStandardMode(true);
-            }
+            this._dismissNormalTutorial();            
             return;
          }
 
@@ -87,11 +83,7 @@ class MainMenu extends ex.UIActor {
          if (slides.length === (tutChallengeIdx + 1)) {
             tutChallengeIdx = 0;
             document.getElementById("tutorial-challenge-next").innerHTML = "Next";
-            this._dismissChallengeTutorial();
-            if (MainMenu._LoadAfterTutorial) {
-               MainMenu._LoadAfterTutorial = false;
-               MainMenu.LoadChallengeMode(true);
-            }
+            this._dismissChallengeTutorial();            
             return;
          }
 
@@ -175,12 +167,22 @@ class MainMenu extends ex.UIActor {
    private _dismissNormalTutorial() {
       removeClass(document.getElementById("tutorial-normal"), "show");
       MainMenu._markTutorialAsDone(GameMode.Standard);
+      if (MainMenu._LoadAfterTutorial) {
+         MainMenu._LoadAfterTutorial = false;
+         MainMenu.LoadStandardMode(true);
+         return;
+      }
       if (gameMode !== GameMode.Standard) MainMenu.LoadStandardMode(true);      
    }
 
    private _dismissChallengeTutorial() {
       removeClass(document.getElementById("tutorial-challenge"), "show");
       MainMenu._markTutorialAsDone(GameMode.Timed);
+      if (MainMenu._LoadAfterTutorial) {
+         MainMenu._LoadAfterTutorial = false;
+         MainMenu.LoadChallengeMode(true);
+         return;
+      }
       if (gameMode !== GameMode.Timed) MainMenu.LoadChallengeMode(true);
    }
 
