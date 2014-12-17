121de7ee27eb5e593ec97e10e32180b55d6f8c74
eonarheim <erik.onarheim@gmail.com>
2014-12-17 14:47:06 -0600

    Fix memory issues that crash the game in IE and WP8, updated Excalibur to latest version to fix some audio problems

--- Src/game/Piece.ts
+++ Src/game/Piece.ts
@@ -8,6 +8,12 @@
 var PieceTypes = [PieceType.Circle, PieceType.Square, PieceType.Triangle, PieceType.Star];
 var PieceTypeToColor = [Palette.PieceColor1, Palette.PieceColor2, Palette.PieceColor3, Palette.PieceColor4];
 var PieceTypeToTexture = [Resources.TextureTile1, Resources.TextureTile2, Resources.TextureTile3, Resources.TextureTile4];
+var PieceTypeToSprites = [
+   [new ex.Sprite(PieceTypeToTexture[0], 0, 0, Config.PieceWidth, Config.PieceHeight), new ex.Sprite(PieceTypeToTexture[0], Config.PieceWidth, 0, Config.PieceWidth, Config.PieceHeight), new ex.Sprite(PieceTypeToTexture[0], Config.PieceWidth * 2, 0, Config.PieceWidth, Config.PieceHeight)],
+   [new ex.Sprite(PieceTypeToTexture[1], 0, 0, Config.PieceWidth, Config.PieceHeight), new ex.Sprite(PieceTypeToTexture[1], Config.PieceWidth, 0, Config.PieceWidth, Config.PieceHeight), new ex.Sprite(PieceTypeToTexture[1], Config.PieceWidth * 2, 0, Config.PieceWidth, Config.PieceHeight)],
+   [new ex.Sprite(PieceTypeToTexture[2], 0, 0, Config.PieceWidth, Config.PieceHeight), new ex.Sprite(PieceTypeToTexture[2], Config.PieceWidth, 0, Config.PieceWidth, Config.PieceHeight), new ex.Sprite(PieceTypeToTexture[2], Config.PieceWidth * 2, 0, Config.PieceWidth, Config.PieceHeight)],
+   [new ex.Sprite(PieceTypeToTexture[3], 0, 0, Config.PieceWidth, Config.PieceHeight), new ex.Sprite(PieceTypeToTexture[3], Config.PieceWidth, 0, Config.PieceWidth, Config.PieceHeight), new ex.Sprite(PieceTypeToTexture[3], Config.PieceWidth * 2, 0, Config.PieceWidth, Config.PieceHeight)]
+];
 
 class PieceEvent extends ex.GameEvent {
    constructor(public cell: Cell) {
@@ -25,17 +31,17 @@ class Piece extends ex.Actor {
    public selected: boolean = false;
 
    public hover: boolean = false;
-   
+
    constructor(id: number, x?: number, y?: number, color?: ex.Color, type?: PieceType) {
       super(x, y, Config.PieceWidth, Config.PieceHeight, color);
       this._id = id;
       this._type = type || PieceType.Circle;
       this._originalColor = color;
       this._updateDrawings();
-      this.calculatedAnchor = new ex.Point(18, 18);
-
+      this.calculatedAnchor = new ex.Point(Config.PieceWidth / 2 * this.scale.x, Config.PieceHeight / 2 * this.scale.y);
+      this.setCenterDrawing(true);
    }
-   
+
    public getId(): number {
       return this._id;
    }
@@ -43,21 +49,25 @@ class Piece extends ex.Actor {
    public getType(): PieceType {
       return this._type;
    }
-   
+
    public setType(type: PieceType): void {
       this._type = type;
       this._updateDrawings();
    }
 
    private _updateDrawings() {
-      var tileSprite = new ex.Sprite(PieceTypeToTexture[this._type], 0, 0, Config.PieceWidth, Config.PieceHeight);
-      this.addDrawing("default", tileSprite);
+      //var tileSprite = new ex.Sprite(PieceTypeToTexture[this._type], 0, 0, Config.PieceWidth, Config.PieceHeight);
+      //this.addDrawing("default", tileSprite);
+      this.addDrawing("default", PieceTypeToSprites[this._type][0]);
 
-      var highlightSprite = new ex.Sprite(PieceTypeToTexture[this._type], Config.PieceWidth, 0, Config.PieceWidth, Config.PieceHeight);
-      this.addDrawing("highlight", highlightSprite);
+      //var highlightSprite = new ex.Sprite(PieceTypeToTexture[this._type], Config.PieceWidth, 0, Config.PieceWidth, Config.PieceHeight);
+      //this.addDrawing("highlight", highlightSprite);
+      this.addDrawing("highlight", PieceTypeToSprites[this._type][1]);
 
-      var fadedSprite = new ex.Sprite(PieceTypeToTexture[this._type], Config.PieceWidth * 2, 0, Config.PieceWidth, Config.PieceHeight);
-      this.addDrawing("faded", fadedSprite);
+      //var fadedSprite = new ex.Sprite(PieceTypeToTexture[this._type], Config.PieceWidth * 2, 0, Config.PieceWidth, Config.PieceHeight);
+      //this.addDrawing("faded", fadedSprite);
+      this.addDrawing("faded", PieceTypeToSprites[this._type][2]);
+      this.setDrawing("default");
    }
 
    public onInitialize(engine: ex.Engine) {
@@ -75,8 +85,10 @@ class Piece extends ex.Actor {
       } else {
          this.setDrawing("default");
       }
+   }
 
-      
+   public dispose() {
+
    }
 }
 
@@ -84,21 +96,20 @@ class PieceFactory {
    private static _maxId: number = 0;
    public static getRandomPiece(x: number = 0, y: number = 0): Piece {
       var index = Math.floor(Math.random() * PieceTypes.length);
-      var piece = new Piece(PieceFactory._maxId++, x, y, PieceTypeToColor[index].clone(), index);
+      var piece = new Piece(PieceFactory._maxId++, x, y, PieceTypeToColor[index], index);
 
-      
+
       game.add(piece);
 
       return piece;
    }
 
    public static getPiece(type: PieceType) {
-      
-      var piece = new Piece(PieceFactory._maxId++, 0, 0, PieceTypeToColor[type].clone(), type);
+
+      var piece = new Piece(PieceFactory._maxId++, 0, 0, PieceTypeToColor[type], type);
 
       game.add(piece);
 
       return piece;
    }
 }
-
